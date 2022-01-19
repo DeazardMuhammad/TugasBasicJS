ac56bd71f960162a687eacfcd9357ba0e5037c09
DeazardMuhammad <85541780+DeazardMuhammad@users.noreply.github.com>
2022-01-19 07:25:57 +0700

    Add files via upload

---
+++ JavaScriptTugas/soal1.js
@@ -0,0 +1,9 @@
+let tanah = 0
+let harga = 1500000
+
+tanah = 20.5*30
+console.log("Luas Tanah = "+ tanah)
+let jumlah = tanah*harga
+console.log("Harga Awal = "+ jumlah)
+let HargaTotal = jumlah*115/100
+console.log("Jumlah Harga = "+ HargaTotal)
