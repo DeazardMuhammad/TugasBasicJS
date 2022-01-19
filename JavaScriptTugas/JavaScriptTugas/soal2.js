03f28944ffe8029c255267e45f19932aff99f05a
DeazardMuhammad <85541780+DeazardMuhammad@users.noreply.github.com>
2022-01-19 07:37:40 +0700

    Delete JavaScriptTugas/JavaScriptTugas directory

--- JavaScriptTugas/JavaScriptTugas/soal2.js
+++
@@ -1,9 +0,0 @@
-let harga = [10000,14000,20000,9000]
-let jumlah = [5,5,2,10]
-let total = 0
-
-for (let index = 0 ; index < harga.length; index++) {
-    total += harga[index]*jumlah[index]        
-    
-}
-console.log("Jumlah Harga = "+total)
--- JavaScriptTugas/JavaScriptTugas/soal4.js
+++
@@ -1,9 +0,0 @@
-let suku1= 4
-let ratio = 3
-
-let total = 0
-for (let total = 10 ; 0 < total.length; total++) {
-    total = 4[total]*3[total]        
-    
-}
-console.log("Total = "+total)
